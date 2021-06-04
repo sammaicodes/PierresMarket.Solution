7eb91f47a9a6246d8364e1c0353ee6fb17f7bb34
Sammai Gutierrez <sammaigtz@hotmail.com>
2021-06-04 16:33:08 -0700

    Add Edit+Add actions  to treat controller.

--- PierresMarket/Controllers/TreatsController.cs
+++ PierresMarket/Controllers/TreatsController.cs
@@ -52,52 +52,52 @@ namespace PierresMarket.Controllers
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
-    // [AllowAnonymous]
-    // public ActionResult Details(int id)
-    // {
-    //   var thisTreat = _db.Treats
-    //       .Include(treat => treat.JoinEntities)
-    //       .ThenInclude(join => join.Treat)
-    //       .FirstOrDefault(treat => treat.TreatId == id);
-    //   return View(thisTreat);
-    // }
+    [AllowAnonymous]
+    public ActionResult Details(int id)
+    {
+      var thisTreat = _db.Treats
+          .Include(treat => treat.JoinEntities)
+          .ThenInclude(join => join.Flavor)
+          .FirstOrDefault(treat => treat.TreatId == id);
+      return View(thisTreat);
+    }
 
-    // public ActionResult Edit(int id)
-    // {
-    //   var thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
-    //   ViewBag.TreatId = new SelectList(_db.Treats, "TreatId", "TreatName");
-    //   return View(thisTreat);
-    // }
+    public ActionResult Edit(int id)
+    {
+      var thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+      ViewBag.FlavorId = new SelectList(_db.Flavors, "FlavorId", "FlavorType");
+      return View(thisTreat);
+    }
 
-    // [HttpPost]
-    // public ActionResult Edit(Treat treat, int TreatId)
-    // {
-    //   if (TreatId != 0)
-    //   {
-    //     _db.TreatFlavor.Add(new TreatFlavor() { TreatId = TreatId, TreatId = treat.TreatId });
-    //   }
-    //   _db.Entry(treat).State = EntityState.Modified;
-    //   _db.SaveChanges();
-    //   return RedirectToAction("Index");
-    // }
+    [HttpPost]
+    public ActionResult Edit(Treat treat, int FlavorId)
+    {
+      if (FlavorId != 0)
+      {
+        _db.TreatFlavor.Add(new TreatFlavor() { FlavorId = FlavorId, TreatId = treat.TreatId });
+      }
+      _db.Entry(treat).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
 
-    // public ActionResult AddTreat(int id)
-    // {
-    //   var thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
-    //   ViewBag.TreatId = new SelectList(_db.Treats, "TreatId", "Name");
-    //   return View(thisTreat);
-    // }
+    public ActionResult AddFlavor(int id)
+    {
+      var thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+      ViewBag.FlavorId = new SelectList(_db.Flavors, "FlavorId", "FlavorType");
+      return View(thisTreat);
+    }
 
-    // [HttpPost]
-    // public ActionResult AddTreat(Treat treat, int TreatId)
-    // {
-    //   if (TreatId != 0)
-    //   {
-    //   _db.TreatFlavor.Add(new TreatFlavor() { TreatId = TreatId, TreatId = treat.TreatId });
-    //   }
-    //   _db.SaveChanges();
-    //   return RedirectToAction("Index");
-    // }
+    [HttpPost]
+    public ActionResult AddFlavor(Treat treat, int FlavorId)
+    {
+      if (FlavorId != 0)
+      {
+      _db.TreatFlavor.Add(new TreatFlavor() { FlavorId = FlavorId, TreatId = treat.TreatId });
+      }
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
 
     // public ActionResult Delete(int id)
     // {
